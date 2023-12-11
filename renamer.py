444a8165e447a71dce1f87bae52c7df77408fe03
redboo <garda1@mail.ru>
2023-12-11 18:49:57 +0500

    refactor

--- renamer.py
+++ renamer.py
@@ -1,53 +1,25 @@
-import os
 import sys
-from glob import glob
+from pathlib import Path
 
 
-def rename_folders_and_files():
-    # Получаем путь к исполняемому файлу
-    executable_path = sys.executable
+def rename_folders_and_files() -> None:
+    current_directory = (
+        Path(sys.executable).parent
+        if __name__ == "__main__"
+        else Path(__file__).resolve().parent
+    )
 
-    # Получаем путь к директории, в которой находится исполняемый файл
-    current_directory = os.path.dirname(executable_path)
-    # Получаем текущую рабочую директорию (где находится скрипт)
-    # current_directory = os.path.dirname(os.path.abspath(__file__))
+    for folder_path in current_directory.iterdir():
+        if folder_path.is_dir():
+            for sub_path in folder_path.iterdir():
+                if sub_path.is_dir():
+                    new_subfolder_path = folder_path / folder_path.name
+                    sub_path.rename(new_subfolder_path)
 
-    for folder in os.listdir(current_directory):
-        folder_path = os.path.join(current_directory, folder)
-
-        if os.path.isdir(folder_path):
-            # Получаем название вложенной папки
-            subfolder_name = os.path.join(folder_path, os.listdir(folder_path)[0])
-
-            # Используем glob для поиска pdf файла внутри папки
-            pdf_files = glob(os.path.join(folder_path, "*.pdf"))
-
-            if pdf_files:
-                # Получаем путь к pdf файлу
-                pdf_file_path = pdf_files[0]
-
-                # Получаем абсолютные пути для переименования
-                new_subfolder_path = os.path.join(folder_path, folder)
-                new_pdf_file_path = os.path.join(folder_path, f"{folder}.pdf")
-
-                # Переименовываем папку
-                os.rename(subfolder_name, new_subfolder_path)
-
-                # Переименовываем pdf файл
-                os.rename(pdf_file_path, new_pdf_file_path)
+                if sub_path.suffix.lower() == ".pdf" and sub_path.is_file():
+                    new_pdf_file = folder_path / f"{folder_path.name}.pdf"
+                    sub_path.rename(new_pdf_file)
 
 
-rename_folders_and_files()
-
-# import os
-
-# # Указываем путь к создаваемой папке (можно указать абсолютный или относительный путь)
-# folder_path = "example"
-
-# # Проверяем, существует ли папка с указанным именем
-# if not os.path.exists(folder_path):
-#     # Создаем папку
-#     os.makedirs(folder_path)
-#     print(f'Папка "{folder_path}" успешно создана.')
-# else:
-#     print(f'Папка "{folder_path}" уже существует.')
+if __name__ == "__main__":
+    rename_folders_and_files()
